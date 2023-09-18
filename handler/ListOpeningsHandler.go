9df1b943d8c5def4ac7a5620194848cbd08803f6
ojosearaujo <jose.aaraujo@mercadolivre.com>
2023-09-18 19:55:05 -0300

    created endpoint of list openings

--- handler/ListOpeningsHandler.go
+++ handler/ListOpeningsHandler.go
@@ -1,12 +1,22 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/odanaraujo/gopportunities/handler/response"
+	"github.com/odanaraujo/gopportunities/schemas"
 	"net/http"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "openings",
-	})
+
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		logger.Errorf("Error on find openings: %v", err)
+		response.SendError(ctx, http.StatusBadRequest, fmt.Sprintf("Error on find openings: %v", err))
+		return
+	}
+
+	response.SendSuccessOk(ctx, "list oppenings", openings)
 }
